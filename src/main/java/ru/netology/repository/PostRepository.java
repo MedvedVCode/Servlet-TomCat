39682fcd12a63ae19b2e9dc03bcdd135e24163fd
Denis Fedorov <ssboshen@yandex.ru>
2022-11-08 12:29:15 +0300

    Add Annotation Config to init() method of Servlet

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,12 +1,13 @@
 package ru.netology.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.netology.model.Post;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
-
+@Repository
 public class PostRepository {
     private long initialMaxCount = 0;
     private AtomicLong maxPostCount = new AtomicLong(initialMaxCount);
